f7e500042c563d1f84994001fa0036c11e338e5c
Lucas Freitas <pro.lucasrfreitas@gmail.com>
2022-03-06 15:43:44 -0300

    refactorate code -- componentize 'transferencias'

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,58 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Transferências'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {
-              print('nheeeaaiuw'); // Add your onPressed code here!
-            },
-          ),
-          body: Column(
-            children: const <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('100.0'),
-                  subtitle: Text('15752'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('100.0'),
-                  subtitle: Text('15752'),
-                ),
-              ),
-            ],
-          ),
-        ),
+            appBar: AppBar(
+              title: const Text('Transferências'),
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () {
+                print('nheeeaaiuw'); // Add your onPressed code here!
+              },
+            ),
+            body: const ListaTransferencias()),
       ),
     );
+
+class ListaTransferencias extends StatelessWidget {
+  const ListaTransferencias({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(transferencia: Transferencia(100, 2053)),
+        ItemTransferencia(transferencia: Transferencia(300, 2053)),
+        ItemTransferencia(transferencia: Transferencia(500, 9053)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia transferencia;
+
+  const ItemTransferencia({required this.transferencia, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(transferencia.valor.toString()),
+        subtitle: Text(transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(
+    this.valor,
+    this.numeroConta,
+  );
+}
