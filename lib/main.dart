741469ddfaed98f2096aecbcb33504af8ce49857
Lucas Freitas <pro.lucasrfreitas@gmail.com>
2022-03-10 02:53:05 -0300

    changing theme

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,13 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.green,
+        ).copyWith(
+          secondary: Colors.blueAccent,
+        ),
+      ),
       home: ListaTransferencias(),
     );
   }
